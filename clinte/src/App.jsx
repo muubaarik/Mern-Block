5ed8c5a347f3c4faebf1f6e5fd504ca8ec85c4c3
Your Name <muubaarik45@gmail.com>
2025-06-27 01:03:42 -0500

    create pages and Routes

--- clinte/src/App.jsx
+++ clinte/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Singin from './pages/singin'
+import Dashboard from './pages/dashboard'
+import Project from './pages/project'
+import Singup from './pages/singup'
+
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+<BrowserRouter>
+   <Routes>
+   <Route path='/' element={<Home/>}></Route>
+   <Route path='/about' element={<About/>}/>
+   <Route path='/sing-in' element={<Singin/>}/>
+   <Route path='/dashboard' element={<Dashboard/>}/>
+   <Route path='/project' element={<Project/>}/>
+   <Route path='/sin-up' element={<Singup/>}/>
+
+   </Routes>
+
+</BrowserRouter>
+    
   )
 }
 
